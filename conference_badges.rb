2d33b6d3dba4123934aa9a1284bc52a7be901748
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 01:52:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,13 @@ def badge_maker(name)
  return "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(array)
+  badges_array = []
+  array.each do |person|
+    badge_maker(person)
+  end
+  badges_array
+end
 def assign_rooms(array)
 end 
 
